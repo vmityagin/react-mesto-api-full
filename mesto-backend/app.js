ffd4f9ec65b3cb2a7235e3c83c041b429443e8c6
vmityagin <mityaginvs@yandex.ru>
2022-09-24 00:20:22 +0300

    fix doubles

--- mesto-backend/app.js
+++ mesto-backend/app.js
@@ -19,12 +19,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true, useUnifiedTopology: true,
 });
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
